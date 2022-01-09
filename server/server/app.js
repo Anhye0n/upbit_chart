6d9a7c4f238bf54374d7c996b7e7f71501456a8e
anhye0n <anhye0ndev@gmail.com>
2022-01-09 16:06:47 +0900

    setting

--- server/server/app.js
+++ server/server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.disable('x-powered-by')
+// app.disable('x-powered-by')
 
 //모듈
 const path = require('path'), favicon = require('serve-favicon')
